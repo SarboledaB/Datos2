50f14cf295f3b427f5d926f01fef466805534711
SarboledaB <35738312+SarboledaB@users.noreply.github.com>
2018-11-06 11:15:52 -0500

    Add files via upload

--- Punto2/Main.java
+++ Punto2/Main.java
@@ -1,74 +1,51 @@
-Packge Main;
+//Codigo hecho en c++ por: https://github.com/ajahuang/UVa/blob/master/UVa%2010154%20-%20Weights%20and%20Measures.cpp
+//codigo hecho en java por: Sebastian Arboleda Botero
 
 import java.util.*;
 
-class Tortuga
-{
-    private int Peso;
-    private int Carga;
+public class Main{
+	
+static Turtle [] T;
+static int[] dp;
+	
 
-    public Tortuga(int Peso,int Carga){
-        this.Peso = Peso;
-        this.Carga = Carga-Peso;
-    }
+public static void solve(){
 
-    public int getPeso(){
-        return Peso;
-    }
+	Arrays.sort(T);
 
-    public int getCarga(){
-        return Carga;
-    }
+    Arrays.fill(dp, dp[5607]);
+    dp[0] = 0;
     
+    int maxNTurtles = 0;
 
- class Main{
-
-    ArrayList<Tortuga> lista=new ArrayList<>();
-    int win = 0;
-
-    public void Tortugas(int p,int c) {
-          Tortuga  Tor = new Tortuga(p,c);
-            lista.add(Tor);
-    }
-
-    public int Dinamico(ArrayList<Tortuga> lista){
-        int campeon=0;
-        int numero=0;
-        if(lista.size()<=0){
-            return 0;
-        }else if(lista.size()==1){
-            return 1;
-        }else{
-            for (int i = 0; i < lista.size()-1; i++) {
-                Tortuga tor = lista.get(i);
-                if(tor.getCarga()>campeon){
-                    campeon= tor.getCarga();
-                }
-                
+    for (int i = 0; i < T.length; ++i){
+        for (int j = maxNTurtles; j >= 0; --j){
+            // Intenta colocar la tortuga i debajo de la pila actual de j tortugas.
+            // 1.Turtle puedo llevar el stack más el mismo.
+            // 2. Producen un peso menor para la pila actual de j + 1 tortugas.
+            if (T[i].s >= dp[j] + T[i].w && T[i].w + dp[j] < dp[j + 1]){
+                dp[j + 1] = dp[j] + T[i].w;
+                maxNTurtles = Math.max(maxNTurtles, j + 1);
             }
-            while (win==0) {
-                win = campeon-win;
-                if(win<0){win=win*-1;}
-                lista.remove(lista.size());
-                numero++;
-                Dinamico(lista);
-            }  
         }
-        return numero;
-    }
+        }
+    
+    System.out.println(maxNTurtles);
+}
 
-
-
-
-    public void main(String[] args) {
-        String entrada = "";
-        Scanner entradaEscaner = new Scanner (System.in);
-        entrada = entradaEscaner.nextLine ();
-        String [] var = entrada.split(" ");
-        int num=0;
-        for(int i=0; i<=var.length-2;i=i+2){
-          Tortugas(Integer.parseInt(var[i]),Integer.parseInt(var[i+1]));
-    }
-    System.out.println(Dinamico(lista));
+public static void main(String []args)
+{
+    @SuppressWarnings("resource")
+   	Scanner reader = new Scanner(System.in);
+         int w = 0;
+         int s = 0;
+         int i = 0;
+        do {			
+         w = reader.nextInt();
+         s   = reader.nextInt();
+         Turtle turtle = new Turtle(w,s);
+         T[i]=turtle;
+         i++;
+        } while (i==5607);
 }
 }
---
+++ Punto2/Turtle.java
@@ -0,0 +1,11 @@
+public class Turtle
+{
+   public int w, s;
+   
+   public Turtle(int w,int s){
+	    this.w = w;
+	    this.s = s;
+	   }
+}
+
+   
